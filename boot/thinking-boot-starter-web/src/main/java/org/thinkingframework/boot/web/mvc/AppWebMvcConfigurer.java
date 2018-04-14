e0c2712da4998ff3a0f811e4b7f530a35e46be8b
henry_yuhm <henry_yuhm@sina.com>
2018-04-14 16:12:19 +0800

    持续优化领域模型和实体

--- boot/thinking-boot-starter-web/src/main/java/org/thinkingframework/boot/web/mvc/AppWebMvcConfigurer.java
+++ boot/thinking-boot-starter-web/src/main/java/org/thinkingframework/boot/web/mvc/AppWebMvcConfigurer.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,25 +13,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppWebMvcConfigurer implements WebMvcConfigurer {
-//    @Override
-//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-//        fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullBooleanAsFalse);
-//
-//        List<MediaType> mediaTypes = new ArrayList<>();
-//        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-//
-//        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-//        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
-//        fastJsonHttpMessageConverter.setSupportedMediaTypes(mediaTypes);
-//
-//        converters.add(fastJsonHttpMessageConverter);
-//    }
-
     @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullBooleanAsFalse);
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect, SerializerFeature.PrettyFormat, SerializerFeature.SortField, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullBooleanAsFalse);
 
         List<MediaType> mediaTypes = new ArrayList<>();
         mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
@@ -40,6 +26,11 @@ public class AppWebMvcConfigurer implements WebMvcConfigurer {
         fastJsonHttpMessageConverter.setSupportedMediaTypes(mediaTypes);
 
         converters.add(fastJsonHttpMessageConverter);
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.removeIf(httpMessageConverter -> httpMessageConverter instanceof MappingJackson2HttpMessageConverter);
     }
 
     @Override
