5b279e84b3f8fba79d2a2e4a6e6491629e59fc36
henry_yuhm <henry_yuhm@sina.com>
2018-04-15 23:04:39 +0800

    持续优化领域模型和实体

---
+++ logistics/task-bundle/src/main/java/org/thinking/logistics/task/bundle/Application.java
@@ -0,0 +1,22 @@
+package org.thinking.logistics.task.bundle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.thinkingframework.boot.web.mvc.AppWebMvcConfigurer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
+@EntityScan(basePackages = "org.thinking.logistics.services.core.entity")
+@EnableJpaRepositories(basePackages = "org.thinking.logistics.services.core.repository")
+public class Application extends AppWebMvcConfigurer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
