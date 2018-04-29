539c2bf6c3a4c9145d767492c083df8c12e3c2ab
henry_yuhm <henry_yuhm@sina.com>
2018-04-29 22:16:31 +0800

    持续优化领域模型和实体

--- logistics/operation-audit/src/main/java/org/thinking/logistics/operation/audit/Application.java
+++ logistics/operation-audit/src/main/java/org/thinking/logistics/operation/audit/Application.java
@@ -3,15 +3,19 @@ package org.thinking.logistics.operation.audit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.thinkingframework.boot.web.mvc.AppWebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
+@EntityScan(basePackages = "org.thinking.logistics.services.core.entity")
+@EnableJpaRepositories(basePackages = "org.thinking.logistics.services.core.repository")
 @Slf4j
 public class Application extends AppWebMvcConfigurer {
     public static void main(String[] args) {
--- logistics/services-core/src/main/java/org/thinking/logistics/services/core/entity/task/WholepiecesTask.java
+++ logistics/services-core/src/main/java/org/thinking/logistics/services/core/entity/task/WholepiecesTask.java
@@ -2,11 +2,13 @@ package org.thinking.logistics.services.core.entity.task;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
 @Entity
 @Data
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class WholepiecesTask extends Task {
 }
